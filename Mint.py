5b27d6516e4831740e889bf6085fb154ba317675
SilasNight <15emiliomurray@gmail.com>
2025-03-12 13:48:01 +0200

    Added two buttons for encrypt and decrypt and added a modified error box

--- Mint.py
+++ Mint.py
@@ -3,11 +3,13 @@ import tkinter as tk
 from tkinter import filedialog
 
 
+
 class Window:
     def __init__(self):
         self.window = tk.Tk()
         self.window.config(padx=20,pady=20)
         self.window.title("Password")
+        self.buttons_canvas = tk.Canvas(self.window)
 
         file_selector_label = tk.Label(self.window, text = "Please select a file:")
         self.file_selection_box = tk.Entry(self.window,width=50)
@@ -15,7 +17,8 @@ class Window:
         password_label = tk.Label(self.window,text = "Please type in your password:")
         self.password_entry = tk.Entry(self.window, )
         spacer = tk.Label(self.window, text = " ")
-        encrypt_button = tk.Button(self.window, text = "Encrypt", command=self.check)
+        encrypt_button = tk.Button(self.buttons_canvas, text = "Encrypt", command=self.encrypt_button)
+        decrypt_button = tk.Button(self.buttons_canvas, text = "Decrypt", command=self.decrypt_button)
 
         file_selector_label.grid(column=0,row=0, sticky = "w")
         self.file_selection_box.grid(column=1,row=0)
@@ -23,7 +26,9 @@ class Window:
         password_label.grid(column=0,row=1, sticky = "w")
         self.password_entry.grid(column = 1, row = 1, columnspan = 2, sticky = "w" + "e")
         spacer.grid(column = 0, row = 2)
-        encrypt_button.grid(column = 0, row = 3, sticky = "w")
+        self.buttons_canvas.grid(column = 0, row = 3,sticky = "w")
+        encrypt_button.grid(column = 0, row = 0)
+        decrypt_button.grid(column = 1, row = 0)
 
         self.window.mainloop()
     def choose(self):
@@ -35,10 +40,38 @@ class Window:
     def check(self):
         item = self.file_selection_box.get()
         if len(item) != 0:
-            self.encypt()
+            return True
         else:
+            return False
+
+    def encrypt_button(self):
+        if self.check():
             pass
-            #error message TODO
+        else:
+            empty_box()
+            # error message TODO
+
+    def decrypt_button(self):
+        if self.check():
+            pass
+        else:
+            empty_box()
+            # error message TODO
+
+
+def empty_box():
+    window = tk.Tk()
+    window.config(padx=20,pady=20)
+    window.title("No file path")
+
+    error_label = tk.Label(window, text= "Please select a file to encrypt/decrypt")
+    ok_button = tk.Button(window, text= "OK", command = window.destroy)
+
+    error_label.grid(column=0, row=0)
+    ok_button.grid(column=0, row=1, sticky="w")
+
+    window.mainloop()
+
 
 
 
