4f5908bd37607a786152f0fc80d91cabb5856482
SilasNight <15emiliomurray@gmail.com>
2025-03-12 11:05:49 +0200

    initial commit

---
+++ Mint.py
@@ -0,0 +1,8 @@
+#A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 0 1 2 3 4 5 6 7 8 9 . , ? ! : ; ' " - _ / \ | ( ) [ ] { } < > + - * / = % & @ # $ ^ ~ `
+import tkinter as tk
+
+
+class Window:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("Password")
